b745fc490aee095e8d5e29191bcd580d34133168
John Reed <johnr@loogatee.com>
2018-04-23 09:50:59 -0700

    comments on make_rootfs.sh that I had not yet saved

--- make_rootfs.sh
+++ make_rootfs.sh
@@ -38,25 +38,26 @@ sudo sed -i "\$aalias dir='ls -CF'" rootfs/root/.bashrc
 #
 
 sudo cp rootfs/etc/apt/sources.list rootfs/etc/apt/sources.listORG 
-
 sudo sed -i "\$adeb http://ftp.us.debian.org/debian/ jessie main contrib non-free" ./rootfs/etc/apt/sources.list
-
-sudo cp rootfs/etc/apt/sources.list rootfs/etc/apt/sources.listJESSIE
+sudo cp rootfs/etc/apt/sources.list    rootfs/etc/apt/sources.listJESSIE
 sudo cp rootfs/etc/apt/sources.listORG rootfs/etc/apt/sources.list
 
 
 #
-#   Need to have the modules
+#   Need to have the modules:  Assumes they're already built and at this location
 #
 sudo rsync -avD /tmp/tmpXX/lib/modules/* rootfs/lib/modules/
 
 
 #
-#    Don't need this anymore
+#    Can remove the original tarball
 #
 rm ./ubuntu-base-14.04.1-core-armhf.tar.gz
 
 
+#
+#    Copy it to the sdcard
+#
 cd rootfs; sudo rsync -avD * /media/johnr/rootfs; cd ..
 
 
